dfe83b330a14856f7c73c5232cd9034be7cf651d
George <georgeasp.pi@gmail.com>
2026-05-06 03:42:42 -0300

    Refactor demo.js for improved luminance processing

--- demo.js
+++ demo.js
@@ -17,33 +17,41 @@ setFunction({
   ],
   glsl: `
     float invLevels = 1.0 / levels;
-    
-    // pixel grid
+  
     vec2 pixelCoord = floor(_st * pixelRes) + 0.5;
     vec2 pixelUV = pixelCoord / pixelRes;
     vec4 colorSample = texture2D(sourceTex, pixelUV);
-
-    // luminância quantizada
+    
     float luma = _luminance(colorSample.rgb);
     float lumaQuant = floor(luma * levels) * invLevels;
 
-    // coordenada
     vec2 localUV = fract(_st * pixelRes);
     localUV.x *= invLevels;
 
-    // deslocamento baseado na luminância
     localUV.x += lumaQuant;
-    
     return localUV;
   `
 })
 
 osc(10, 0.1, 0.1)
-  .modulate(noise(4), 0.5)
+  .modulate(noise(2), 0.5)
+  //.modulate(noise(4), 0.5)
   .rotate(() => a.fft[2] * 0.5)
   .out(o1)
 
 
+src(s1)
+  .lumaSlice(o1, () => a.fft[1] * 10 + 35, 7) 
+  .scale(() => 1 - (a.fft[3] * 0.2)) 
+  .colorama(() => a.fft[1] * 0.5)
+  .hue(() => time * 0.3 + a.fft[2]) 
+  .color(2, 1, 2)
+  .brightness(() => a.fft[2] * 0.1)
+  .contrast(1.4)
+  .out(o2)
+
+
+/* I'm not sure what to do
 src(s1)
   .lumaSlice(o1, () => a.fft[1] * 30 + 20, 7) 
   .scale(() => 1 - (a.fft[0] * 0.4)) 
@@ -53,5 +61,6 @@ src(s1)
   .brightness(() => a.fft[1] * 0.2)
   .contrast(1.4)
   .out(o2)
+ */
 
 render(o2)
